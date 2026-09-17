ae92eb410228fed4f00b1c64ed95b0bc75592c1f
sushanth05-dev <sushanth051208@gmail.com>
2026-09-17 15:57:35 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,95 @@
+# Printing Messages & Basic print()
+
+print("Hello World!")
+print("Welcome to Python")
+print("Hemanth")
+print("I am learning Python")
+print("Python Programming")
+print("Good Morning")
+print("Welcome Students")
+print("Python is easy")
+print("My First Program")
+print("Hello from Python!")
+
+# Quotes, Messages vs Calculations & Syntax
+
+print("2+5")
+print(2+5)
+print("10-3")
+print(10-3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello")
+print("10+20")
+print(10+20)
+
+# Variables
+
+name = "Hemanth"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+print(city)
+score = 95
+print(score)
+x=10
+print(x)
+message = "Welcome"
+print(message)
+a = 10
+b = 20
+print(a+b)
+quantity = 5
+price = 10 
+print(quantity*price)
+
+# Data Types: int, float, str
+
+int
+float
+str
+age = 21
+height = 5.8
+name = "Arun"
+age = 20
+price = 99.5
+name = "Ravi"
+marks = 90, int
+temperature = 36.5, float
+message = "Welcome", str
+
+# Sequence of Instructions
+
+print("Start")
+print("MIddle")
+print("Third")
+x = 10
+print(x)
+x = 20
+print(x)
+a = 5
+print(a)
+a = 10
+print(a)
+print("A")
+print("B")
+print("C")
+name = "Ravi"
+age = 20
+print(name)
+print(age)
+print("1")
+print("2")
+print("3")
+print("4")
+x = 5
+y = 10
+print(x)
+print(y)
+print("Start")
+print(2+3)
+print("Login")
+print("Dashboard")
+print("Logout")
